c4873da14b6c1d75e305b6806e5631cf0a03c14f
stOneskull <stOneskull@gmail.com>
2023-07-09 00:15:12 +1000

    refactoring + couple lil bugs fixed

--- README.md
+++ README.md
@@ -1,37 +1,41 @@
+# Godchat
+
 a bit of fun playing with simpleaichat - https://github.com/minimaxir/simpleaichat
 
 choose a character to talk to and save your sessions to a text file
 
-steps:
 
-1. get an openai account..https://platform.openai.com/
+- get an openai account.. https://platform.openai.com/
 
-2. once you have an openai account and are logged in, you can find the option for API keys in the Personal menu at the top-right of the webpage
+- once you have an openai account and are logged in, you can find the option for API keys in the Personal menu at the top-right of the webpage
 
-3. once you have an API key, make a folder for the chatz
+- once you have an API key, make a folder for the chatz..
 
+```
 mkdir godschat
 
 cd godschat
 
 python3 -m venv venv
-
 or: python -m venv venv
 
-source venv/bin/activate
-
+Linux: source venv/bin/activate
 Windows: venv\Scripts\activate
 
 pip install simpleaichat
 
 git clone https://github.com/stOneskull/godschat.git
 
-add a line to your ~/.bashrc file: export OPENAI_API_KEY=put_your_key_here
+Linux: add a line to your ~/.bashrc file:
+    export OPENAI_API_KEY=put_your_key_here
 
 Windows: setx OPENAI_API_KEY put_your_key_here
+
 
 cd godschat 
 
 python godschat.py
 
+
 deactivate
+```
--- godschat.py
+++ godschat.py
@@ -8,97 +8,135 @@ from simpleaichat import AIChat as ai
 from godsdata import allgods
 
 
-openapi_key = os.environ.get('OPENAI_API_KEY')
+openapi_key = os.environ.get("OPENAI_API_KEY")
 
 
-Heart = True 
+Heart = True
 
 
 def clr(lines=99):
     print("\n" * lines)
-    
+
 
 def bye():
     global Heart
-    print('''
+    print(
+        """
     <q>uit
     <n>ew session
-     ''')
-    if 'q' in input('? ').lower():
+     """
+    )
+    if "q" in input("? ").lower():
         Heart = False
 
 
-def logwork():
+def getlog():
     try:
-        with open('log.txt', 'r') as logger:
-            loglist = logger.readlines()
+        with open("log.txt", "r") as logger:
+            return logger.readlines()
     except:
-        print('no saved log')
+        print("no saved log")
         sleep(1)
-        return menu
 
-    print('''
-    <b>rowse for god
-    <e>nter god
-    ''')
 
-    if 'e' in input('? ').lower():
-        findgod = input("make god log of who? ")
-
-    else:
-        findgod, _ = search()
-        
-    sessions = {}
-
-    seshlines = []
-    seshes = []
-
-    for line in loglist:
-        if 'START SESSION' in line:
-            if seshlines:
-                seshes.append(seshlines)
-                seshlines = []
-
-        seshlines.append(line)
-
-    if seshlines:
-        seshes.append(seshlines)
-
-    for sesh in seshes:
-        session = sesh[0].rstrip()
-        god = sesh[1].split(':')[0]
-
-        if god not in sessions:
-            sessions[god] = {}
-
-        sessions[god][session] = sesh
-
-    goddict = sessions[findgod]
-
-    with open(findgod+'.txt', 'w') as godlog:
-        for entry in goddict:
-            for line in goddict[entry]:
+def writelog(goddict, god):
+    with open(god + ".txt", "w") as godlog:
+        for entry in goddict[god]:
+            for line in goddict[god][entry]:
                 godlog.writelines(line)
 
-    print(f'log for {findgod} saved as {findgod}.txt')
+    print(f"log for {god} saved as {god}.txt")
     sleep(2)
 
     return menu
 
 
+def make_sessions_dict(sessionslist, focus):
+    sessions = {}
+
+    if focus:
+        sessions[focus] = {'focus': []}
+
+    for sessionlist in sessionslist:
+        if focus:
+            focusedlist = [line for line in sessionlist if focus+':' in line]
+            focusedlist.append('\n')
+            sessions[focus]['focus'].extend(focusedlist)
+            continue
+
+        session = sessionlist[0].rstrip()
+        god = sessionlist[1].split(":")[0]
+
+        if god not in sessions:
+            sessions[god] = {}
+
+        sessions[god][session] = sessionlist
+
+    return sessions
+
+
+def make_sessions_list(loglist):
+    sessionlines = []
+    sessionslist = []
+
+    for line in loglist:
+        if "START SESSION" in line:
+            if sessionlines:
+                sessionslist.append(sessionlines)
+                sessionlines = []
+
+        sessionlines.append(line)
+
+    if sessionlines:
+        sessionslist.append(sessionlines)
+
+    return sessionslist
+
+
+def logmenu():
+    loglist = getlog()
+
+    if not loglist:
+        return menu
+
+    print(
+        """
+    <b>rowse for god
+    <e>nter godname exactly
+    <l>og your prompts
+    """
+    )
+
+    focus = None
+    thelog = input("? ").lower()
+
+    if thelog == "e":
+        findgod = input("make god log of whom? ")
+    elif thelog == "l":
+        focus = findgod = "YOU"
+    else:
+        findgod, _ = search()
+
+    sessionslist = make_sessions_list(loglist)
+    goddict = make_sessions_dict(sessionslist, focus)
+
+    sleep(2)
+    writelog(goddict, findgod)
+
+
 def set_temperature():
-    temperature = round(float(
-        input('\n\nEnter creativity level from 0 to 10: ')
-        ))
-    
+    try:
+        temperature = round(float(
+            input("\n\nEnter creativity level from 0 to 10: ")))
+    except:
+        temperature = 5
+
     if temperature:
         temperature /= 10
-    
-    if temperature > 1:
+
+    if not 0 <= temperature <= 1:
         temperature = 1
 
-    print(temperature)
-    sleep(1)
     return float(temperature)
 
 
@@ -108,54 +146,53 @@ def chat(god, description):
     params = {"temperature": set_temperature()}
 
     chatz = ai(
-        god, description, params=params, 
-        api_key=openapi_key, console=not logging
-        )
-    
+        god, description, params=params, api_key=openapi_key, console=not logging
+    )
+
     if not logging:
         return chatz
-    
+
     print('Type "quitz" to exit chat..\n\n\n')
-    
-    response = chatz('hi')
+
+    response = chatz("hi")
     pretty = f"{god}: {response}\n"
     print(pretty)
 
-    with open('session', 'r') as session:
+    with open("session", "r") as session:
         i = session.readline().strip()
 
-    with open('log.txt', 'a') as logger:
-        logger.write(f'START SESSION {i}\n')
+    with open("log.txt", "a") as logger:
+        logger.write(f"START SESSION {i}\n")
         logger.write(pretty)
 
     while Heart is True:
-        prompt = input('YOU: ')
+        prompt = input("YOU: ")
         response = chatz(prompt)
         pretty = f"{god}: {response}\n"
         print(pretty)
 
-        with open('log.txt', 'a') as logger:
-            logger.write(f'YOU: {prompt}\n')
+        with open("log.txt", "a") as logger:
+            logger.write(f"YOU: {prompt}\n")
             logger.write(pretty)
 
-        if 'quitz' in prompt.lower():
-            with open('log.txt', 'a') as logger:
-                logger.write('\n\n')
+        if "quitz" in prompt.lower():
+            with open("log.txt", "a") as logger:
+                logger.write("\n\n")
             return bye
-        
-        #todo add savesession option
+
+        # todo add savesession option
 
 
 def updategods():
     global allgods
 
     try:
-        with open('usergods.json', 'r') as jar:
+        with open("usergods.json", "r") as jar:
             usergods = json.load(jar)
     except:
         usergods = {}
 
-    allgods['usergods'] = usergods
+    allgods["usergods"] = usergods
 
 
 def search():
@@ -169,7 +206,7 @@ def search():
     for i, godtype in enumerate(categories):
         print(i, godtype)
 
-    choose = input('\nEnter number: ')
+    choose = input("\nEnter number: ")
 
     godtype = categories[int(choose)]
 
@@ -200,25 +237,24 @@ def search():
 def usermade():
     global allgods
 
-    god = input('Choose your character: ')
-    description = input('Give a short description or extra notes: ')
+    god = input("Choose your character: ")
+    description = input("Give a short description or extra notes: ")
+    savegod = input("Save your character for future? (y/n) ")
 
-    savegod = (input('Save your character for future? (y/n) '))
+    if "n" not in savegod.lower():
+        usergods = allgods["usergods"]
 
-    if 'n' not in savegod.lower():
-        usergods = allgods['usergods']
-
-        category = input('Enter category for your character: ')
+        category = input("Enter category for your character: ")
 
         if category not in usergods:
             usergods[category] = {}
 
         usergods[category][god] = description
 
-        with open('usergods.json', 'w') as jar:
+        with open("usergods.json", "w") as jar:
             json.dump(usergods, jar)
 
-        allgods['usergods'] = usergods
+        allgods["usergods"] = usergods
 
     return chat(god, description)
 
@@ -226,59 +262,60 @@ def usermade():
 def menu():
     updategods()
     clr()
-    print('c', 'choose god')
-    print('r', 'random choice')
-    print('u', 'unique choice')
-    print('m', 'make god log')
-    print('x', 'exit')
+    print("c", "choose god")
+    print("r", "random choice")
+    print("u", "unique choice")
+    print("m", "make god log")
+    print("x", "exit")
 
     choose = input("\nEnter a choice: ")
 
-    if choose == 'm':
-        return logwork
-    
-    if choose == 'x':
+    if choose == "m":
+        return logmenu
+
+    if choose == "x":
         return bye
-    
-    if choose == 'u':
+
+    if choose == "u":
         return usermade
-    
-    if choose == 'r':
+
+    if choose == "r":
         godtype = choice(list(allgods.keys()))
         civtype = choice(list(allgods[godtype].keys()))
         god = choice(list(allgods[godtype][civtype].keys()))
         description = allgods[godtype][civtype][god]
         return chat(god, description)
-    
+
     return chat(*search())
-    
+
 
 def setup():
     global logging
     clr()
 
-    logging = not 'n' in input(
-        "save your session text? (y/n) : ").lower()
+    logging = not "n" in input("save your session text? (y/n) : ").lower()
 
     if logging:
         try:
-            with open('session', 'r') as session:
+            with open("session", "r") as session:
                 i = session.readline().strip()
                 i = int(i) + 1
-            with open('session', 'w') as session:
+            with open("session", "w") as session:
                 session.write(str(i))
         except:
-            with open('session', 'w') as first:
-                first.write('1')
+            with open("session", "w") as first:
+                first.write("1")
 
     return menu
 
 
 def intro():
     clr()
-    input("""
+    input(
+        """
     High! Welcome to Godchat..
-    Press enter to continue..""")
+    Press enter to continue.."""
+    )
     return setup
 
 
@@ -288,7 +325,7 @@ def bounce(boing):
             boing = boing()
         except:
             boing = intro
-    
+
 
 if __name__ == "__main__":
     bounce(intro)
